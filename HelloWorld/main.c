72f182d56575b03769cb7e465e7eb472ec3803bf
nevvisk <r.simmerling@gmx.de>
2024-11-10 00:10:57 +0100

    playin around in HelloWorld file

--- HelloWorld/main.c
+++ HelloWorld/main.c
@@ -2,11 +2,16 @@
 
 int main () {
 	const char* testing[] = {"does", "this", "even", "work", "?", NULL};
+	const int NUM1 = 3;
+	const int NUM2 = 5;
 	printf("Hello World!\n");
 	char **c = testing; 
 	while (*c != NULL) {
 		printf("%s\n", *c);
 		++c;
 	}
+	printf("Adding %d and %d\n", NUM1, NUM2);
+	int result = NUM1 + NUM2;	
+	printf("%d\n", result);
 	return 0;
 }
